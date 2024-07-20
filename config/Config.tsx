3b26ade9b21980b84aa216d1475470e969061bee
Groovinder <dkristiant555@gmail.com>
2024-07-20 22:09:22 +1000

    Config removed from git ignore

---
+++ config/Config.tsx
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBOLEp5ya1Tl6ShH2mqI2rWk9ZM6YTZYGk",
+  authDomain: "homeworkfl-f8367.firebaseapp.com",
+  projectId: "homeworkfl-f8367",
+  storageBucket: "homeworkfl-f8367.appspot.com",
+  messagingSenderId: "992499928348",
+  appId: "1:992499928348:web:93b09fc97e2baf681afed9"
+};
